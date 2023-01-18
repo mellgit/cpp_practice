6102d5001c47de9672a2799b54a89adb76c01b92
mellgit <daniilhorn8@gmail.com>
2023-01-18 23:41:45 +0300

    refactoring

--- app/src.sh
+++ app/src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-g++ -std=c++17 arr_main.cpp -o arr_main # 17++
+g++ -std=c++17 function_prototypes.cpp -o function_prototypes # 17++
 # # g++ arr_main.cpp -o arr_main # 97++
-./arr_main
-rm arr_main
+./function_prototypes
+rm function_prototypes
 
