82ab8f6ec0f2be162fc5f41d0256b7aedfd01e95
mellgit <daniilhorn8@gmail.com>
2023-01-18 23:24:51 +0300

    refactoring

--- app/src.sh
+++ app/src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# g++ -std=c++17 arr_main.cpp -o arr_main # 17++
+g++ -std=c++17 arr_main.cpp -o arr_main # 17++
 # # g++ arr_main.cpp -o arr_main # 97++
-# ./arr_main
-# rm arr_main
+./arr_main
+rm arr_main
 
