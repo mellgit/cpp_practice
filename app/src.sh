3ff6735608ff01507a792af8fc9746a63abad272
mellgit <daniilhorn8@gmail.com>
2023-01-22 18:46:59 +0300

    refactoring

--- app/src.sh
+++ app/src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-g++ -std=c++17 function_prototypes.cpp -o function_prototypes # 17++
+g++ -std=c++17 dyn_mem.cpp -o dyn_mem # 17++
 # # g++ arr_main.cpp -o arr_main # 97++
-./function_prototypes
-rm function_prototypes
+./dyn_mem
+rm dyn_mem
 
