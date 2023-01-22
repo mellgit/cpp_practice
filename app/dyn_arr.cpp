f87df47c7db29aae50bf546de62378f0efc2699c
mellgit <daniilhorn8@gmail.com>
2023-01-22 21:18:06 +0300

    double dynamic array

--- app/dyn_arr.cpp
+++ app/dyn_arr.cpp
@@ -2,16 +2,65 @@
 using namespace std;
 
 void dynamic_array();
-
+void double_dynamic_array();
 
 
 int main(){
 
-    dynamic_array();
+    // dynamic_array();
+
+    double_dynamic_array();
 
 
     return 0;
 }
+
+void double_dynamic_array(){
+    /*
+    понятия двумерных массивов нет в с++
+    по факту создается одномерный массив, каждый элемент указывает на другой одномерный массив
+    те двумерный массив это одномерный массив одномерных массивов
+    */
+
+
+    // создание и выделения памяти массива
+    int row; cout<<"enter row: ";cin>>row; 
+    int col; cout<<"enter col: ";cin>>col; 
+
+    int **arr = new int* [row]; // создание массива указателей
+
+    for (int i=0; i<row; i++){
+        // создание массивоа для каждого указателя основного массива
+        arr[i] = new int[col];
+    }
+
+    // заполнение двумерного массива
+    for (int i=0; i<row; i++){
+        for (int j=0; j<col; j++){
+            arr[i][j] = rand()%20;
+        }
+    }
+    // вывод двумерного массива
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout<<endl;
+    }
+
+    // удаление и очистка массива
+    for (int i=0; i<row; i++){
+        // удаление каждого массива, по указателю из основного массива
+        delete [] arr[i]; 
+        arr[i] = nullptr;
+    }
+
+    delete [] arr; // удаление основного массива, в котором хранятся указатели на другие массивы
+    arr = nullptr;
+}
+
 
 void dynamic_array(){
     /*
