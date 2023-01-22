7a5f601a33760ea0efb4d742124f23aa193aa302
mellgit <daniilhorn8@gmail.com>
2023-01-22 17:18:53 +0300

    function template

--- app/function_prototypes.cpp
+++ app/function_prototypes.cpp
@@ -7,15 +7,36 @@ using namespace std;
 void test_prototypes(string name);
 void default_args(int a, int b=2) ;
 
+// шаблонная функция, может работать только с одним типом (int or double)
+// template <class T> из c++
+template <typename T> // из c (разницы нет)
+// void sum(T a, T b) - можно не возвращать значения
+T sum(T a, T b)
+{
+    return a+b;
+}
+
+// шалон может работать одновременно с разными типами (int and double)
+template <typename T1, typename T2>
+T1 sub(T1 a, T2 b)
+{
+    return a - b;
+}
+
 int main()
 {
-    string name = "Bob";
-    test_prototypes(name);
+    // string name = "Bob";
+    // test_prototypes(name);
     
-    
-    int a = 5;
-    default_args(a);
-    default_args(a, 3);
+    // int a = 5;
+    // default_args(a);
+    // default_args(a, 3);
+
+    cout<<sum(5,9)<<endl;
+    cout << sum(5.3, 9.4) << endl;
+
+    cout << sub(5, 9.3) << endl;
+
     return 0;
 }
 
