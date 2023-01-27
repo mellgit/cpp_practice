2c7da6aabdb42172380f3e23e7b4750d300c8222
mellgit <daniilhorn8@gmail.com>
2023-01-27 22:53:25 +0300

    refactoring

--- app/src.sh
+++ app/src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-g++ -std=c++17 string_work.cpp -o string_work # 17++
+g++ -std=c++17 proc.cpp -o proc # 17++
 # # g++ arr_main.cpp -o arr_main # 97++
-./string_work
-rm string_work
+./proc
+rm proc
 
