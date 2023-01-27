92268c031b2cf6d7515e72fbde2ecb60a2ff42ea
mellgit <daniilhorn8@gmail.com>
2023-01-27 23:52:38 +0300

    conditional compilation

--- app/proc.cpp
+++ app/proc.cpp
@@ -3,18 +3,60 @@
 // #define begin {
 // #define end }
 #define sum(x,y) (x*y)
+// #define DEBUG
+#define VALUE 6
 
 using namespace std;
 
 
 void prepoccesing();
-
+void conditional_compilation();
 
 int main(){
 
-    prepoccesing();
+    // prepoccesing();
+    conditional_compilation();
     return 0;
 }
+
+void conditional_compilation(){
+    /*
+    условная компиляция - если директива define определена,
+    то код внутри ifdef и endif будет выполнятся, иначе нет
+    можно использовать в отладке, те при подключенной define DEBUG 
+    будет выводиться отладочная информация, а без ее подключения 
+    отладочной информации не будет 
+
+    #else - если директива определена, то работает ifdef, 
+    иначе отработает #else
+
+    #if, #elif, #else - работают точно так же как и if, elif, else
+    только на уровне препроцессинга
+    */
+#ifdef DEBUG
+    cout<<"begin cicle"<<endl;
+#else
+    cout<<"DEBUG off"<<endl;
+#endif
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+#ifdef DEBUG
+    cout << "end cicle" << endl;
+#endif
+
+#if VALUE == 1
+    cout<<"value > 1"<<endl;
+#elif VALUE == 5
+    cout << "value > 5" << endl;
+#else
+    cout<<"idk what the value"<<endl;
+#endif
+}
+
 
 void prepoccesing(){
     /*
@@ -25,7 +67,7 @@ void prepoccesing(){
     */
 
     cout<<PI<<endl;
-    // begin and end вместо фигурных скобок
+    // begin и end вместо фигурных скобок
     // for (int i = 0; i < 6; i++)
     // begin
     //     cout<<i<<endl;
