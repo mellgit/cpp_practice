0bb37accae90cd1c6608da027a541e7d86602767
mellgit <daniilhorn8@gmail.com>
2023-01-29 00:16:16 +0300

    just class

---
+++ app/oop/just_oop.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+
+class Human
+{
+public:
+    int age;
+    string name;
+};
+
+void my_class()
+{
+    Human first_human;
+    cout << "first human" << endl;
+    first_human.age = 23;
+    first_human.name = "bob";
+    cout << first_human.age << endl;
+    cout << first_human.name << endl;
+
+    cout << "_________________" << endl;
+
+    Human second_human;
+    cout << "second human" << endl;
+    second_human.age = 53;
+    second_human.name = "alice";
+    cout << second_human.age << endl;
+    cout << second_human.name << endl;
+}
+
+int main()
+{
+    my_class();    
+
+    return 0;
+}
+
+
