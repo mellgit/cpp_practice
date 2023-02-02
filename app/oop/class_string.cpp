6c96af348507642043e635bb54a37e4f21ea6e84
mellgit <daniilhorn8@gmail.com>
2023-02-03 00:27:21 +0300

    create assignment operator

--- app/oop/class_string.cpp
+++ app/oop/class_string.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 // создание класса string 45:51
-class My_class
+class My_string
 {
 private:
     char *str;
 public:
-    My_class(char *str)
+    My_string(char *str)
     {
         int len = strlen(str);
-        cout<<&str<<endl;
+        // cout<<&str<<endl; // 0x16fba7778 - разныеячейки памяти
         cout<<"len str: "<<len<<endl;
         this->str = new char[len+1]; // новый символьный массив с +1 значением под '\0'
 
@@ -20,17 +20,40 @@ public:
         }
         this->str[len] = '\0'; // добавить конец сторки в строку
 
-        cout<<&this->str<<endl;
+        // cout<<&this->str<<endl; // 0x16fba77f0 - разные ячейки памяти
         
     }
-    My_class()
+    My_string()
     {
         str = nullptr;
     }
-    ~My_class()
+    ~My_string()
     {
         delete [] this->str;
         this->str = nullptr;
+    }
+
+    My_string & operator =(const My_string & other)
+    {
+        if (this->str != nullptr)
+        {
+            delete [] str;
+        }
+        // код из конструктора необходимо перенесте в private фу-ю
+        int len = strlen(str);
+        this->str = new char[len + 1];
+
+        for (int i = 0; i < len; i++)
+        {
+            this->str[i] = str[i];
+        }
+        this->str[len] = '\0';
+
+        return *this;
+    }
+    void print()
+    {
+        cout<<str;
     }
 };
 
@@ -41,6 +64,12 @@ public:
 int main()
 {
     cout<<"============ BEGIN ============"<<endl;
-    My_class str("test");
+    My_string str("hello");
+    My_string str2("world!!!");
+
+    str = str2;
+    
+
+    cout<<endl;
     return 0;
 }
