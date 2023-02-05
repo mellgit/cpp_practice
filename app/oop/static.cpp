dbd2b62956ed10dc6e3823d241848082fb60907d
mellgit <daniilhorn8@gmail.com>
2023-02-05 23:22:54 +0300

    static methods

--- app/oop/static.cpp
+++ app/oop/static.cpp
@@ -17,10 +17,11 @@ private:
     int weight;
     string color;
     int id;
+    static int count; // статическая переменная - для всех объектов класса одинаковая
 
 public:
 
-    static int count; // статическая переменная - для всех объектов класса одинаковая 
+    
 
     Apple(int weight, string color)
     {
@@ -34,6 +35,29 @@ public:
     {
         return id;
     }
+    string get_color()
+    {
+        return color;
+    }
+
+    static int get_count()
+    {
+        /*
+        статический геттер метод, дабы защитить экземпляр count
+        this указатель работать не может, тк это указатель для конкретного объекта класса
+        а static относится сразу ко всем объектам
+        */
+        return count;
+    }
+
+    static void change_color(Apple &apple, string color)
+    {
+        /*
+        чтобы статический метод работал с конкретным объектом класса
+        в параметры статического метода необходимо передать, ссылку на объект
+        */
+        apple.color = color;
+    }
     
 
 };
@@ -45,12 +69,18 @@ void static_method()
     Apple apple(123, "green");
     Apple apple2(123, "red");
     Apple apple3(123, "yellow");
-    
 
-    cout<<"counter: "<<Apple::count<<endl; // вывод значения статического экземпляра класса
+    // вывод значения статического экземпляра класса
+    // если counter был бы в public секции
+    // cout<<"counter: "<<Apple::count<<endl;
 
+    cout<<Apple::get_count()<<endl;
     cout<<"id"<<endl;
     cout << apple.get_id() << endl;
     cout << apple2.get_id() << endl;
     cout << apple3.get_id() << endl;
+
+    cout << "befor change: " << apple.get_color() << endl;
+    Apple::change_color(apple, "blue");
+    cout << "after change: " << apple.get_color() << endl;
 }
