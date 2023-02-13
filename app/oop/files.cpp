ea8f7127d80f4ef3daeb92aa10ccc32c4b79141b
mellgit <daniilhorn8@gmail.com>
2023-02-14 00:13:13 +0300

    ifstream

--- app/oop/files.cpp
+++ app/oop/files.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void of_file();
+void if_file();
 
 int main()
 {
@@ -11,12 +12,52 @@ int main()
     // ifstream - чтение файла
     // ofstrem - запись в файл
 
-    of_file();
+    // of_file();
+    if_file();
 
     return 0;
 }
 
 
+void if_file()
+{
+    string path_to_file = "data/my_file.txt";
+
+    ifstream fin;
+    fin.open(path_to_file);
+
+    if (!fin.is_open())
+        cout<<"error open file"<<endl;
+    else
+        cout<<"--- file is open ---"<<endl;
+    
+
+    
+    // char ch;
+    // fin.get(ch) - посимвольной считывание
+    // если считать получилось - return true -> else return false
+    // поэтому данный метод можно положить в цикл while
+    // while (fin.get(ch))
+    // {
+    //     cout<<ch;
+    // }
+
+    
+    string str;
+    // fin.eof() - чтение до конца файла
+    while (!fin.eof())
+    {
+        // fin >> str; // c getline работать удобнее
+        getline(fin, str); // построчная запись
+        cout<<str<<endl;
+    }
+
+    cout<<endl;
+    fin.close(); 
+}
+
+
+ 
 void of_file()
 {
     string path_to_file = "data/my_file.txt";
