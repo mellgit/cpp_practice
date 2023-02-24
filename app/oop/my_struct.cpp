b0b9c7473c22a323b0054e4bddc97e7768038e7b
mellgit <daniilhorn8@gmail.com>
2023-02-24 21:03:53 +0300

    smart pointer

--- app/oop/my_struct.cpp
+++ app/oop/my_struct.cpp
@@ -4,12 +4,60 @@ using namespace std;
 
 
 void my_struct();
+void smart_pointers();
 
 int main()
 {
-    my_struct();
+    // my_struct();
+    smart_pointers();
     return 0;
 }
+
+template<typename T>
+class SmartPointer
+{
+public:
+
+    SmartPointer(T *ptr)
+    {
+        this->ptr = ptr;
+        cout << "constructor" << endl;
+    }
+    ~SmartPointer()
+    {
+        delete ptr;
+        cout << "destructor" << endl;
+    }
+
+    T& operator *()
+    {
+        /*
+        чтобы разименовать умный указатель, необходимо перегрузить
+        указатель (*)
+        */
+        return *ptr;
+    }
+private:
+    T *ptr;
+};
+
+void smart_pointers()
+{
+    /*
+    умные указатели - примитивные сборщики мусора
+    */
+
+    // обычные указатели
+    // int *ptr = new int(3);
+    // delete ptr;
+    // ptr = nullptr;
+
+    SmartPointer<int> pointer = new int(4); // похож на обычный указатель
+    cout << *pointer << endl; // использование перегрузки разименования
+}
+
+
+
 
 class MyClass
 {
