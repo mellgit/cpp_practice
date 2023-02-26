a73ee0e4421fa882ac288115f8c806ba2779f37d
mellgit <daniilhorn8@gmail.com>
2023-02-26 17:04:38 +0300

    more smart pointer

--- app/oop/my_struct.cpp
+++ app/oop/my_struct.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 #include <string>
+#include <memory> // для работы с умными указателями
 using namespace std;
 
 
 void my_struct();
 void smart_pointers();
+void new_smart_pointer();
+
 
 int main()
 {
     // my_struct();
-    smart_pointers();
+    // smart_pointers();
+    new_smart_pointer();
     return 0;
+}
+
+
+void new_smart_pointer()
+{
+    /*
+    auto_ptr - устарел и похож на unique_ptr (в 17 стандарте не работает)
+    unique_ptr
+    shared_ptr
+    */
+
+    // два указателя ссылаются на одну область памяти
+    // unique_ptr<int> p1(new int(5));
+    // unique_ptr<int> p2;
+
+    // очистить указатель
+    // p1.reset(); // удаляет указатель и данные из выделенной памяти
+    // p1.release(); // удаляет только указатель
+
+    // возвращает указатель
+    // int *p = p1.get(); 
+    // cout<<*p<<endl;
+
+    
+    // p2 = move(p1); // 1 вариант
+    // p2.swap(p1); // 2 вариант
+
+    // часто используемый вариант
+    shared_ptr<int> p1(new int(5));
+    shared_ptr<int> p2(p1);
 }
 
 template<typename T>
