2c0876983ac4b4c8e8f02179fbd0f4c3daf3070f
mellgit <daniilhorn8@gmail.com>
2023-02-27 23:25:09 +0300

    error: declaration of T shadows template parameter

--- app/oop/dynamic_structures/singly_linked_list.cpp
+++ app/oop/dynamic_structures/singly_linked_list.cpp
@@ -2,8 +2,80 @@
 #include <string>
 using namespace std;
 
+// template <typename T> class node;
+// template <typename T> void swap(node<T> & a, node<T> & b);
+
+// template <typename T> class List;
+// template <typename T> class Node;
+template <typename T>;
+
+template <typename T>
+class List
+{
+
+public:
+    List(/* args */);
+    ~List();
+
+    void push_back(T data); // добавление данных в конец списка
+
+private:
+    
+    template<typename T>
+    class Node 
+    {
+    public:
+        Node *pNext; // адрес, по которому лежат данные
+        T data;
+        Node(T data=T(), Node *pNext=nullptr)
+        {
+            /*
+             / *pNext=nullptr - если добавлять в конец списка, тк указателя на нем не будет
+             data=T() - таким способом, в data не будет лежать мусор
+            */
+            this->data = data;
+            this->pNext = pNext;
+
+        }
+    };
+    
+    int size; // количество элементов списка
+    Node<T> *head; // первый элемент списка
+};
+
+template <typename T>
+List<T>::List(/* args */)
+{
+    size = 0;
+    head = nullptr;
+}
+
+template <typename T>
+List<T>::~List()
+{
+}
+
+template <typename T>
+void List<T>::push_back(T data)
+{
+    if (head == nullptr)
+    {
+        head = new Node<T>(data);
+    }
+    else
+    {
+
+    }
+}
+
 int main()
 {
-    
+    /*
+    односвязные списки
+    */
+
+    List<int> lst;
+    lst.push_back(5);
+        
     return 0;
-}
+} 
