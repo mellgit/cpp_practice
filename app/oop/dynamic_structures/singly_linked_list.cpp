253c8c08cc36614281ef7d49a2e2ea606b802138
mellgit <daniilhorn8@gmail.com>
2023-02-28 00:22:55 +0300

    fix error for template

--- app/oop/dynamic_structures/singly_linked_list.cpp
+++ app/oop/dynamic_structures/singly_linked_list.cpp
@@ -2,9 +2,24 @@
 #include <string>
 using namespace std;
 
+template <typename T>
+class Node
+{
+public:
+    Node *pNext; // адрес, по которому лежат данные
+    T data;
+    Node(T data = T(), Node *pNext = nullptr)
+    {
+        /*
+         / *pNext=nullptr - если добавлять в конец списка, тк указателя на нем не будет
+         data=T() - таким способом, в data не будет лежать мусор
+        */
+        this->data = data;
+        this->pNext = pNext;
+    }
+};
 
 
-// error: declaration of 'T' shadows template parameter
 template <typename T>
 class List
 {
@@ -14,27 +29,16 @@ public:
     ~List();
 
     void push_back(T data); // добавление данных в конец списка
+    int get_size(){return size;}
+
+    T& operator[](const int index);
 
 private:
-    
-    template<typename T>
-    class Node 
-    {
-    public:
-        Node *pNext; // адрес, по которому лежат данные
-        T data;
-        Node(T data=T(), Node *pNext=nullptr)
-        {
-            /*
-             / *pNext=nullptr - если добавлять в конец списка, тк указателя на нем не будет
-             data=T() - таким способом, в data не будет лежать мусор
-            */
-            this->data = data;
-            this->pNext = pNext;
+    /*
+    в pivate должен быть класс Node, но выкидывает ошибку: error: declaration of 'T' shadows template parameter
+    как вытащил из класса List, ошибки пропала
+    */
 
-        }
-    };
-    
     int size; // количество элементов списка
     Node<T> *head; // первый элемент списка
 };
@@ -54,13 +58,50 @@ List<T>::~List()
 template <typename T>
 void List<T>::push_back(T data)
 {
+    // если данных нет
     if (head == nullptr)
     {
         head = new Node<T>(data);
+        
     }
+
+    // если данные уже есть
     else
     {
+        Node<T> *current = this->head; // временная переменная
 
+        // пока не найдем элемент со значением nullptr
+        while (current->pNext != nullptr)
+        {
+            current = current->pNext;
+        }
+        current->pNext = new Node<T>(data); // добавить в конец списка
+
+    }
+
+    size++;
+}
+
+template <typename T>
+T &List<T>::operator[](const int index)
+{
+    /*
+    перегрузка оперетора [], чтобы получить данные из списка
+    */
+    int counter = 0;
+    Node<T> *current = this->head; // временная переменная
+
+    while (current != nullptr)
+    {
+        // если индекс нашелся
+        if (counter == index)
+        {
+            return current->data;
+        }
+
+        // если индекс не тот, переходим на следующий
+        current = current->pNext;
+        counter++;
     }
 }
 
@@ -70,8 +111,26 @@ int main()
     односвязные списки
     */
 
+    cout<<"========================= BEGIN ========================="<<endl;
+
     List<int> lst;
-    lst.push_back(5);
-        
+    // lst.push_back(5);
+    // lst.push_back(15);
+    // lst.push_back(25);
+
+    int value_for_size;
+    cout<<"enter list size: "; cin>>value_for_size;
+    for (int i = 0; i < value_for_size; i++)
+    {
+        lst.push_back(rand()%20);
+    }
+    
+
+    for (int i = 0; i < lst.get_size(); i++)
+    {
+        cout<<"elem: "<<lst[i]<<endl;
+    }
+    
+
     return 0;
 } 
