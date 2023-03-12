d27d9c981ada7c8c783d2daf547c67b72f7cff95
mellgit <daniilhorn8@gmail.com>
2023-03-12 21:28:10 +0300

    operator auto

---
+++ app/oop/dynamic_structures/stl_dir/list_stl.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    /*
+    доп инфо
+    auto - автоматическое присваение типа данных
+    auto a = 10; -> int
+    auto b = 1.343; -> double
+    auto b; -> error: всегда должно быть значение, иначе ошибка
+    на простых переменных, лучше не использовать из за читабельности
+
+    лучше присваивать значение итератора вектора, тк запись сокращается
+    auto it = my_vector.begin();
+    вместо
+    vaector<int>::iterator it = my_vector.begin();
+
+    */
+    // list_funtction();
+    return 0;
+}
