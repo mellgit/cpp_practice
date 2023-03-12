dafdad6be5dd7c5e69b349d93980436d8961b5cf
mellgit <daniilhorn8@gmail.com>
2023-03-12 21:50:53 +0300

    list

--- app/oop/dynamic_structures/stl_dir/list_stl.cpp
+++ app/oop/dynamic_structures/stl_dir/list_stl.cpp
@@ -4,7 +4,71 @@
 using namespace std;
 
 
+template<typename T>
+void print_list(const list<T> &lst)
+{
+    for (auto i = lst.cbegin(); i != lst.cend(); ++i) // префикс
+    {
+        cout<<*i<<endl;
+    }
+    
+}
 
+void list_funtction()
+{
+    /*
+    list - структура, которая  быстро работает на добавлене\удаление эл-ов
+    в list нет перегрузки [], используется только итератор
+    */
+
+    // list<int> my_list;
+    list<int> my_list = {23,435,6};
+    my_list.push_back(34);
+    my_list.push_front(5);
+
+    auto it = my_list.begin(); // итератор указанный на первый эл-т листа
+
+    my_list.sort(); // сортировка от меньшего с большему
+    my_list.pop_back(); // удаляет последний эл-т
+    my_list.pop_front(); // удаляет первый эл-т
+    my_list.size(); // размер листа
+    my_list.unique();  // удаляет последовательные дубликаты
+    my_list.reverse(); // обратный порядок значений
+    my_list.clear(); // очистка листа
+    my_list.remove(44); // удаляет по значению
+
+    auto it2 = my_list.begin();
+    my_list.insert(it2, 232); // вставка в лист
+
+    /*
+    проиххвольный индекс
+    it2++; - работает
+    it2+=3; - не работает, лист не поддерживает
+    вопрос решит advance()
+
+    auto it2 = my_list.begin();
+    advance(it2, 3);
+    my_list.insert(it2, 232); // вставка в лист произвольного индекса
+    */
+
+    // my_list.erase(it2); // удаление
+    my_list.assign(4, 343434); // заполненный список одинаковыми значениями
+
+    /*
+    копирование(перезапись) листа
+    list<int> my_list = {23,435,6};
+    list<int> my_list2 = {323,4435,56};
+    my_list.assign(my_list2.begin(), my_list2.end());
+    */
+
+    print_list(my_list);
+
+    // вывод листа
+    // for (auto i = my_list.begin(); i != my_list.end(); i++) // постфикс
+    // {
+    //     cout<<*i<<endl;
+    // }
+}
 
 
 int main()
@@ -23,6 +87,6 @@ int main()
     vaector<int>::iterator it = my_vector.begin();
 
     */
-    // list_funtction();
+    list_funtction();
     return 0;
 }
