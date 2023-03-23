8386d10e59c9a08849112fe32f84f94d08019237
mellgit <daniilhorn8@gmail.com>
2023-03-23 23:21:37 +0300

    pragma once

--- app/oop/file_project/my_class.h
+++ app/oop/file_project/my_class.h
@@ -1,4 +1,29 @@
 #pragma once
+/*
+pragma once - защита от повторного включения (вложенность a.h в b.h + b.h в a.h), 
+так же можно использовать условную компиляцию
+тк это работает для всех компиляторов
+
+
+пример условной компиляции - работает точно также как и pragma once
+ifndef - if not defined (если не определено)
+
+#ifndef _MY_CLASS_H_
+#define _MY_CLASS_H_
+
+#include <iostream>
+namespace my_ns 
+{
+    class my_class
+    {
+    public:
+        void print_msg(char str[]);   
+    };
+}
+
+#endif
+
+*/
 namespace my_ns 
 {
     class my_class
