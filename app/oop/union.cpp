83c11a6a188931f74e25e722398b55a078fcfca3
mellgit <daniilhorn8@gmail.com>
2023-03-26 22:19:15 +0300

    anonymous function

--- app/oop/union.cpp
+++ app/oop/union.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 #include <string>
 #include <functional>
+#include <vector>
 using namespace std;
+
+
+void test_dowork(vector<int> &vc, function<void(int)> func)
+{
+    /*
+    вызывает ф-ю func столько раз, сколько элементо в vc
+    */
+    for (auto elem : vc)
+    {
+        func(elem);
+    }
+}
+
+void anonym_function()
+{
+    /*
+    анонимные ф-и, синтаксис - [](){};
+    у анон ф-и нет имени, вызывается через объект function, либо напрямую
+    есть два варианта инициализации анонимной ф-и (вариант 1 и 2)
+
+    у анон ф-и свое пространство имен, в нем не видны переменные и тп из другого пространства
+    для этого необходимо в [] передать необходимые параметры по ссылке(вариант 3)
+    в 14 стандарте есть возможность вызавать через auto
+    auto f = [](){};
+
+    тип анон ф-и определяется автоматичиски, если нет return - void,
+    инача отталкивается от типа возвращаемого значения
+    return 1; - int
+    return 55.667; - float
+    return true; - bool
+    */
+    vector<int> vc = {234,564,8634,475};
+    
+    // 1 вариант
+    function<void(int)> f;
+    f = [](int a){cout<<"call anonym function a = "<<a<<endl;};
+    test_dowork(vc, f);
+    cout<<"========================"<<endl;
+    // 2 вариант
+    test_dowork(vc, [](int a){
+        cout<<"call anonym function a = "<<a<<endl;
+    });
+
+    // 3 вариант
+    int p = 0;
+    cout<<p<<endl;
+    auto f2 = [&p]()
+    {
+        p = 4;
+    };
+    f2();
+    cout<<p<<endl;
+
+}
+
 
 void foo()
 {
@@ -60,6 +116,7 @@ void union_fun()
 int main()
 {
     // union_fun();
-    std_function();
+    // std_function();
+    anonym_function();
     return 0;
 }
