92d56b4a238a833ac2516e65a7634e7e1b675406
mellgit <daniilhorn8@gmail.com>
2023-03-26 22:32:55 +0300

    return value from thread by anonymos function

--- app/oop/dynamic_structures/thread/thr_value.cpp
+++ app/oop/dynamic_structures/thread/thr_value.cpp
@@ -4,6 +4,40 @@
 using namespace std;
 
 
+int sum(int a, int b)
+{
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    
+    cout << " thr_id="<< this_thread::get_id() << " ==== sum start ====" << endl;
+    
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    
+    cout << " thr_id="<< this_thread::get_id() << " ==== sum end ====" << endl;
+    
+    return a+b;
+    
+}
+
+void anon_fun_thread()
+{
+    /*
+    вернуть значение из потока при помощи анонимной ф-и
+    */
+
+    int result;
+    thread t([&result](){result = sum(3,6);});
+
+    for (size_t i = 0; i < 10; i++) 
+    {
+        this_thread::sleep_for(chrono::milliseconds(500)); 
+        cout << this_thread::get_id() << " - thr_fun " << i << endl; // номер потока
+    }
+
+    t.join();
+    cout<<"result = "<<result<<endl;
+
+}
+
 void do_work(int &a)
 {
     this_thread::sleep_for(chrono::milliseconds(1000));
@@ -52,6 +86,7 @@ void return_thr_value()
 
 int main()
 {
-    return_thr_value();
+    // return_thr_value();
+    anon_fun_thread();
     return 0;
 }
