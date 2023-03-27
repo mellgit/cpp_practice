e06bddf23efa850ef4dcb0794898b91885332b32
mellgit <daniilhorn8@gmail.com>
2023-03-27 23:29:19 +0300

    time measurement

--- app/oop/dynamic_structures/thread/thr_value.cpp
+++ app/oop/dynamic_structures/thread/thr_value.cpp
@@ -4,6 +4,91 @@
 using namespace std;
 
 
+int sum_for_time_measurement(int a, int b)
+{
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    
+    cout << " thr_id="<< this_thread::get_id() << " ==== sum start ====" << endl;
+    
+    this_thread::sleep_for(chrono::milliseconds(5000));
+    
+    cout << " thr_id="<< this_thread::get_id() << " ==== sum end ====" << endl;
+    
+    return a+b;   
+}
+
+class SimpleTimer
+{
+public:
+    /*
+        чтобы не писать постоянно счетчики времени, проще описать класс для таймера
+        при объявлении объекта класса SimpleTimer в ф-и, запуститься таймер
+        как только ф-я закончит свое действие вызовится деструктор, в котором будет результат выполнения
+
+    */
+    SimpleTimer()
+    {
+        start = chrono::high_resolution_clock::now();
+    }
+    ~SimpleTimer()
+    {
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        cout<<"duration time: "<<duration.count()<<"s"<<endl;
+    }
+private:
+    // std::chrono::_V2::system_clock::time_point std::chrono::_V2::system_clock::now() - что возвращает метдо now()
+    // chrono::time_point<chrono::steady_clock> start, end; // неотработало
+    chrono::_V2::system_clock::time_point start, end; // отработало
+};
+
+void time_measurement()
+{
+    /*
+    измерение времени работы программы
+    в #include <thread> уже есть chrono, если потоки не нужны, то можно использовать только chrono
+
+    начало отсчета - auto start = chrono::high_resolution_clock::now();
+    конец отсчета - auto end = chrono::high_resolution_clock::now();
+    разница - chrono::duration<float> duration = end - start;
+    */
+    auto start = chrono::high_resolution_clock::now();
+
+    int result;
+    // тк sum_for_time_measurement выполняется в другом потоке, работать все будет быстрее чем в одном потоке
+    thread t([&result](){result = sum_for_time_measurement(3,6);}); // 7 sec
+    // result = sum_for_time_measurement(3,6); // 12 sec
+
+    for (size_t i = 0; i < 10; i++) 
+    {
+        this_thread::sleep_for(chrono::milliseconds(500)); 
+        cout << this_thread::get_id() << " - thr_fun " << i << endl; // номер потока
+    }
+
+    t.join();
+    cout<<"result = "<<result<<endl;
+    
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<float> duration = end - start;
+    cout<<"time program: "<<duration.count()<<"s"<<endl;
+
+    //! using timer from class
+    SimpleTimer st; 
+    cout<<"using timer from class"<<endl;
+    int result2;
+    thread t2([&result2](){result2 = sum_for_time_measurement(3,6);}); 
+
+    for (size_t i = 0; i < 10; i++) 
+    {
+        this_thread::sleep_for(chrono::milliseconds(500)); 
+        cout << this_thread::get_id() << " - thr_fun " << i << endl; // номер потока
+    }
+
+    t2.join();
+    cout<<"result = "<<result2<<endl;
+
+}
+
 int sum(int a, int b)
 {
     this_thread::sleep_for(chrono::milliseconds(1000));
@@ -87,6 +172,7 @@ void return_thr_value()
 int main()
 {
     // return_thr_value();
-    anon_fun_thread();
+    // anon_fun_thread();
+    time_measurement();
     return 0;
 }
