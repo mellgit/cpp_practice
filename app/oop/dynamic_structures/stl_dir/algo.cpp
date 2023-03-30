809018385bc6310e33f14c2073140ecd9e07467a
mellgit <daniilhorn8@gmail.com>
2023-03-31 00:14:21 +0300

    find, find_if, find_if_not

--- app/oop/dynamic_structures/stl_dir/algo.cpp
+++ app/oop/dynamic_structures/stl_dir/algo.cpp
@@ -33,6 +33,92 @@ public:
     }
 };
 
+
+void find_fun()
+{
+    /*
+    find, find_if, find_if_not
+    find - найти элемент в коллекции, возвращает итератор элемента
+    find_if - возвращает true, если элемент в наборе найден по определенным условиям
+    find_if_not - возвращает true, если предикат возвращает false(обратный fing_if)
+    */
+    vector<int> v = {34,5,76,234,45,98};
+
+    cout<<" === find === "<<endl;
+    // если result будет указывать на мусор(конец набора), элемент не найден
+    auto result = find(v.begin(), v.end(), 5232);
+    
+    if (result == v.end())
+    {
+        cout<<"not found"<<endl;
+    }
+    else
+    {
+        cout<<"found"<<endl;
+    }
+
+
+    vector<int> v2 = {34,6,234,98}; // четные
+    // vector<int> v2 = {343,63,235,93}; // нечетные
+
+    cout<<" === find_if === "<<endl;
+    auto result2 = find_if(v2.begin(), v2.end(), [](int a)
+    {
+        // проверка на то, что в наборе только четные числа
+        return a%2==0;
+    });
+    
+    if (result2 == v2.end())
+    {
+        cout<<"only odd"<<endl;
+    }
+    else
+    {
+        cout<<"only even"<<endl;
+    }
+    
+
+    vector<int> v3 = {-333,-34,-6,-234,-98}; 
+    cout<<" === find_if_not === "<<endl;
+    auto result3 = find_if_not(v3.begin(), v3.end(), [](int a)
+    {
+        return a<0;
+    });
+    
+    if (result3 == v3.end())
+    {
+        cout<<"all > 0"<<endl;
+    }
+    else
+    {
+        cout<<"< 0"<<endl;
+    }
+
+    cout<<"===== class Person ====="<<endl;
+    // лучше не перегружать оператор сравнения, тк больше напряга, проще предикатом
+    vector<Person> person{
+        Person("bob", 232),
+        Person("alice", 32),
+        Person("john", 22),
+        Person("mike", 172),
+    };
+
+    auto result4 = find_if(person.begin(), person.end(),[](const Person &p)
+    {
+        // return p.name == "mike";
+        return p.name == "mike" && p.score == 172; // поиск по нескольким кретериям
+    });
+    
+    if (result4 == person.end())
+    {
+        cout<<"person not found"<<endl;
+    }
+    else
+    {
+        cout<<"person found"<<endl;
+    }
+}
+
 void algo_fun()
 {
     /*
@@ -108,6 +194,7 @@ void algo_fun()
 
 int main()
 {
-    algo_fun();
+    // algo_fun();
+    find_fun();
     return 0;
 }
