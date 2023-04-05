60411b827868991078e267436300b1ce154f84bc
mellgit <daniilhorn8@gmail.com>
2023-04-05 23:13:58 +0300

    dead lock

--- app/oop/dynamic_structures/thread/thr_mutex.cpp
+++ app/oop/dynamic_structures/thread/thr_mutex.cpp
@@ -7,31 +7,92 @@ using namespace std;
 
 mutex mtx;
 
+mutex mtx1;
+mutex mtx2;
 
-class SimpleTimer
+void print_1(char ch)
 {
-public:
     /*
-        чтобы не писать постоянно счетчики времени, проще описать класс для таймера
-        при объявлении объекта класса SimpleTimer в ф-и, запуститься таймер
-        как только ф-я закончит свое действие вызовится деструктор, в котором будет результат выполнения
+    this_thread::sleep_for(chrono::milliseconds(1)); - остановка на одну милисекунду нужна
+    для того, чтобы поток 2 успел взять данные для потока 1, иначе не получить взяимной блокировки
+    тк поток 1 все равно запускается раньше, но это работает не всегда
+    программа может, как заработать, так и не заработать 
 
+    проблема взаимной блокировки
+    mtx2.lock();
+    this_thread::sleep_for(chrono::milliseconds(1));
+    mtx1.lock();
+
+    решение взаимной блокировки (выполнен правильный порядок захвата mtx)
+    mtx1.lock();
+    this_thread::sleep_for(chrono::milliseconds(1));
+    mtx2.lock();
     */
-    SimpleTimer()
+    mtx1.lock();
+    this_thread::sleep_for(chrono::milliseconds(1));
+    mtx2.lock();
+
+    
+    for (int i=0; i<=5; i++)
     {
-        start = chrono::high_resolution_clock::now();
+        for (int j=0; j<=5; j++)
+        {
+            cout<<ch;
+            this_thread::sleep_for(chrono::milliseconds(20));
+        }   
+        cout<<endl;
     }
-    ~SimpleTimer()
+    
+    cout<<endl;
+    mtx1.unlock();
+    mtx2.unlock();
+
+}
+
+void print_2(char ch)
+{
+    /*
+    
+    */
+    mtx1.lock();
+    this_thread::sleep_for(chrono::milliseconds(1));
+    mtx2.lock();
+
+    
+    for (int i=0; i<=5; i++)
     {
-        end = chrono::high_resolution_clock::now();
-        chrono::duration<float> duration = end - start;
-        cout<<"duration time: "<<duration.count()<<"s"<<endl;
+        for (int j=0; j<=5; j++)
+        {
+            cout<<ch;
+            this_thread::sleep_for(chrono::milliseconds(20));
+        }   
+        cout<<endl;
     }
-private:
-    // std::chrono::_V2::system_clock::time_point std::chrono::_V2::system_clock::now() - что возвращает метдо now()
-    // chrono::time_point<chrono::steady_clock> start, end; // неотработало
-    chrono::_V2::system_clock::time_point start, end; // отработало
-};
+    
+    cout<<endl;
+    mtx1.unlock();
+    mtx2.unlock();
+
+}
+
+void dead_lock_mtx()
+{
+    /*
+    dead lock - взаимная блокировка, когда поток 1 взял данные потока 2, а поток 2 взял данные потока 1
+    и они оба не могу начать действовать, тк нет необходимых данных для работы
+
+    чтобы избежать такой проблемы, необходимо заранее позаботиться о том, какой поток
+    в какой момент времени будет делать, те сначала дать данные потока 1 на потока 1, 
+    а потом данные потока 2 на поток 2
+    те, соблюдать порядок захвата mtx
+    */
+    thread t(print_1, '#');
+    thread t2(print_2, '@');
+
+    t.join();
+    t2.join();
+    
+}
 
 
 void print(char ch)
@@ -88,7 +149,6 @@ void lock_guard_mutex()
 
     после открытия mutex всегда необходимо его закрыть, класс lock_guard решает эту проблему
     */
-    SimpleTimer st;
     thread t(print_lock_guard, '#');
     thread t2(print_lock_guard, '@');
 
@@ -117,9 +177,38 @@ void mutex_fun()
     t2.join();
 }
 
+
+class SimpleTimer
+{
+public:
+    /*
+        чтобы не писать постоянно счетчики времени, проще описать класс для таймера
+        при объявлении объекта класса SimpleTimer в ф-и, запуститься таймер
+        как только ф-я закончит свое действие вызовится деструктор, в котором будет результат выполнения
+
+    */
+    SimpleTimer()
+    {
+        start = chrono::high_resolution_clock::now();
+    }
+    ~SimpleTimer()
+    {
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<float> duration = end - start;
+        cout<<"duration time: "<<duration.count()<<"s"<<endl;
+    }
+private:
+    // std::chrono::_V2::system_clock::time_point std::chrono::_V2::system_clock::now() - что возвращает метдо now()
+    // chrono::time_point<chrono::steady_clock> start, end; // неотработало
+    chrono::_V2::system_clock::time_point start, end; // отработало
+};
+
+
 int main()
 {
+    SimpleTimer st;
     // mutex_fun();
-    lock_guard_mutex();
+    // lock_guard_mutex();
+    dead_lock_mtx();
     return 0;
 }
